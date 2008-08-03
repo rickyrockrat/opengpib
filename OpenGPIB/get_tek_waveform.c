dc2db08be479b055248b061051054aaaaec42133
RickyRockRat <gpib@rickyrockrat.com>
2008-08-03 06:19:59 +0000

    Added multiple channel reads and cursor reads
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@11 fe987565-7590-435f-b057-69d0534b0db8

--- OpenGPIB/get_tek_waveform.c
+++ OpenGPIB/get_tek_waveform.c
@@ -8,10 +8,20 @@ scope, then dump that into a file.
 */ /************************************************************************
 Change Log: \n
 $Log: not supported by cvs2svn $
+Revision 1.1  2008/08/02 08:53:58  dfs
+Initial working rev
+
 */
  
 #include "serial.h"
+#include "common.h"
 #include <time.h>
+
+#define MAX_CHANNELS 7
+#define CURSORS      6 /**offset into CH_LIST where cursors keyword is */
+#define BUF_SIZE 8096
+char buf[BUF_SIZE];
+
 #if 0
 struct prologix_port {
 	char phys_name[MAX_PORT_NAME];  /* physical device name of this interface, i.e the /dev/xxx. */
@@ -33,9 +43,33 @@ struct gpib_handle *open_gpib(struct gpib_port *port)
 }
 
 #endif
-#define BUF_SIZE 8096
-char buf[BUF_SIZE];
 
+char *CH_LIST[MAX_CHANNELS]=\
+{
+	"CH1",
+	"CH2",
+	"REF1",
+	"REF2",
+	"REF3",
+	"REF4",
+	"CURSORS",
+};
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int check_channel(char *ch)
+{
+	int i;
+	if(NULL ==ch)
+		return 1;
+	for (i=0;i<MAX_CHANNELS;++i){
+		if(!strcmp(ch,CH_LIST[i]))
+			return 0;
+	}
+	return 1;
+}
 /***************************************************************************/
 /** .
 \n\b Arguments:
@@ -203,7 +237,7 @@ int write_string(struct serial_port *p, char *msg, int len)
     printf("Error Writing to %s\n",p->phys_name);
 		return -1;
   }
-	printf("wrote %d bytes\n%s\n",rtn,msg);
+	/*printf("wrote %d bytes\n%s\n",rtn,msg); */
 	if(rtn != i)
 		printf("Write mis-match %d != %d\n",rtn,i);
 	usleep(500); 
@@ -222,7 +256,7 @@ void verify(struct serial_port *p, char *msg)
 	i=sprintf(buf,"%s\r",msg);
 	write_string(p,buf,i);
 	read_string(p,buf,BUF_SIZE);
-	printf("%s = %s\n",msg,buf);	
+	/*printf("%s = %s\n",msg,buf);	 */
 }
 /***************************************************************************/
 /** .
@@ -243,7 +277,7 @@ int init_prologix(struct serial_port *p, int inst_addr)
 		printf("%s:Unable to read from port on ver\n",__func__);
 		return -1;
 	}
-	printf("Got %d bytes\n",i);
+	/*printf("Got %d bytes\n",i); */
 	if(NULL == strstr(buf,"Prologix")){
 		printf("%s:Unable to find correct ver in\n%s\n",__func__,buf);
 		return -1;
@@ -271,7 +305,20 @@ int init_prologix(struct serial_port *p, int inst_addr)
 \n\b Arguments:
 \n\b Returns:
 ****************************************************************************/
-int init_instrument(struct serial_port *p, int channel)	 
+int set_channel(struct serial_port *p,char *ch)
+{
+	int i;
+	if(NULL == ch)
+		return -1;
+	i=sprintf(buf,"DATA SOURCE:%s\r",ch);
+	return write_string(p,buf,i);	
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int init_instrument(struct serial_port *p)	 
 {
 	int i;
 	printf("Initializing Instrument\n");
@@ -280,15 +327,12 @@ int init_instrument(struct serial_port *p, int channel)
 		printf("%s:Unable to read from port on id\n",__func__);
 		return -1;
 	}
-	printf("Got %d bytes\n",i);
+	/*printf("Got %d bytes\n",i); */
 	if(NULL == strstr(buf,"TEK/2440")){
 		printf("Unable to find 'TEK/2440' in id string '%s'\n",buf);
 		return -1;
 	}
-	write_string(p,"DATA ENCDG:ASCI\r",0);
-	i=sprintf(buf,"DATA SOURCE:CH%d\r",channel);
-	write_string(p,buf,i);
-	return 0;
+	return write_string(p,"DATA ENCDG:ASCI\r",0);
 }
 
 /***************************************************************************/
@@ -304,6 +348,61 @@ int get_data(struct serial_port *p)
 	return i;
 }
 
+/***************************************************************************/
+/** read the cursors data.
+To get the cursor info, just type
+cursor?
+The return string is something like:
+CURSOR FUNCTION:VOLTS,TARGET:CH1,UNITS:TIME:BASE,UNITS:SLOPE:BASE,UNITS:VOLTS:BA
+SE,REFVOLTS:UNITS:V,REFVOLTS:VALUE:1.0000,REFSLOPE:XUNIT:SEC,REFSLOPE:YUNIT:V,RE
+FSLOPE:VALUE:1.0000,REFTIME:UNITS:SEC,REFTIME:VALUE:1.0000,XPOS:ONE:3.00,XPOS:TW
+O:-3.00,YPOS:ONE:2.47,YPOS:TWO:1.38,TPOS:ONE:1.24000E+2,TPOS:TWO:1.95500E+2,MODE
+:DELTA,SELECT:TWO
+Then look for function (VOLTS or?) target (CH1)
+Send the command to read target: 
+ch1?
+Data looks like:
+CH1 VOLTS:5E-1,VARIABLE:0,POSITION:-3.15,COUPLING:DC,FIFTY:OFF,INVERT:OFF
+
+Then you take the YPOS:ONE and YPOS:TWO and subtract, then multiply by the CH1 VOLTS: value.
+This example gives you 545 mV.
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int read_cursors(struct serial_port *p,int fd)
+{
+	char *t,lbuf[100];
+	float one,two,volts, diff;
+	int i;
+	
+	printf("Reading Cursor\n");
+	write_string(p,"CURSOR?\r",0);
+	read_string(p,buf,BUF_SIZE);
+	t=get_string("TARGET",buf);
+	printf("Target=%s\n",t);
+	one=get_value("YPOS:ONE",buf);
+	two=get_value("YPOS:TWO",buf);
+	printf("Target=%s, one=%E, two=%E\n",t,one,two);
+	i=sprintf(lbuf,"%s?\r",t);
+	write_string(p,lbuf,i);
+	read_string(p,buf,BUF_SIZE);
+	volts=get_value("VOLTS",buf);
+	diff=one-two;
+	if(diff <0){
+		diff *=-1;
+		i=1; /**swap one-two  */
+	}else
+		i=0;
+		
+	diff*=volts;
+	one *=volts;
+	two *=volts;
+	printf("volts=%E, diff=%E\n",volts,diff);
+	i=sprintf(buf,"%F %F %F\n",i?two:one,i?one:two,diff);
+	/*write(fd,buf,i); written out by loop*/
+	
+	return i;
+}
 
 /***************************************************************************/
 /** .
@@ -314,12 +413,16 @@ void usage(void)
 {
 	printf("get_tek_waveform <options>\n"
 	" -a addr set instrument address to addr (2)\n"
-	" -c n Use channel n for data (1)\n"
+	" -c n Use channel n for data (CH1). CH1, CH2, REF1-4, and CURSORS\n"
+	"    The -c option can be used multiple times. In this case, the fname\n"
+	"    is a base name, and the filename will have a .1 or .2, etc appended\n"
 	" -o fname put output to file called fname\n"
 	" -p path set path to serial port name\n"
+	" -r read cursors and place in ofname.cursor\n"
 	
 	"");
 }
+
 /***************************************************************************/
 /** .
 \n\b Arguments:
@@ -328,36 +431,57 @@ void usage(void)
 int main(int argc, char * argv[])
 {
 	struct serial_port *p;
-	char *name, *ofname;
-	int c,inst_addr,channel, rtn, ofd;
+	char *name, *ofname, *channel[MAX_CHANNELS], *lbuf;
+	int i, c,inst_addr, rtn, ofd, ch_idx;
 	name="/dev/ttyUSB0";
-	c=1;
 	inst_addr=2;
-	channel=1;
+	channel[0]="CH1";
+	for (c=1;c<MAX_CHANNELS;++c){
+		channel[c] = NULL;
+	}
+	ch_idx=0;
 	rtn=1;
 	ofname=NULL;
-	while( -1 != (c = getopt(argc, argv, "a:c:o:p:")) ) {
+	while( -1 != (c = getopt(argc, argv, "a:c:ho:p:")) ) {
 		switch(c){
 			case 'a':
 				inst_addr=atoi(optarg);
 				break;
 			case 'c':
-				channel=atoi(optarg);
+				if(ch_idx>=5){
+					printf("Too many -c options. Max of %d allowed.\n",MAX_CHANNELS);
+				}
+				channel[ch_idx++]=strdup(optarg);
 				break;
+			case 'h':
+				usage();
+				return 1;
 			case 'o':
 				ofname=strdup(optarg);
 				break;
 			case 'p':
 				name=strdup(optarg);
 				break;
+			default:
+				usage();
+				return 1;
 		}
 	}
-	if(NULL == ofname)
+	if(NULL == ofname){
 		ofd=1;
-	else	if(0>(ofd=open(ofname,O_RDWR|O_CREAT,S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR))) {
-		printf("Unable to open '%s' for writing\n",ofname);
-		return 1;
 	}
+		
+	if(0 == ch_idx)	/**Use default, and one channel  */
+		++ch_idx;	
+	else {
+		for (c=0;c<ch_idx;++c){
+			if(check_channel(channel[c])){
+				printf("Channel %s is invalid\n",channel[c]);
+				return 1;
+			}
+		}
+	}
+	
 	if(NULL == (p=open_serial_port(name,115200,0,0,0))){
 		printf("Can't open %s. Fatal\n",name);
 		return 1;
@@ -368,15 +492,56 @@ int main(int argc, char * argv[])
 		goto closem;
 	}
 	
-	if(-1 == init_instrument(p,channel)){
+	if(-1 == init_instrument(p)){
 		printf("Unable to initialize instrument\n");
 		goto closem;
 	}
-	if(-1 == (c=get_data(p)) ){
-		printf("Unable to get waveform??\n");
-		goto closem;
+	if(NULL != ofname){
+		/**find max strlen of channel name.  */
+		for (i=c=0;c<ch_idx;++c){
+			int x;
+			x=0;
+			if(NULL != channel[c])
+				x=strlen(channel[c]);
+			if(x>i)
+				i=x;
+		}
+		i+=5;
+		/**allocate buffer for all fnames.  */
+		if(NULL == (lbuf=malloc(strlen(ofname)+i)) ){
+			printf("Out of mem for lbuf alloc\n");
+			goto closem;
+		}
+	}else
+		lbuf=NULL;
+
+	/**channel loop. Open file, dump data, close file, repeat  */
+	for (c=0;c<ch_idx && NULL != channel[c];++c){
+		if(NULL != ofname && NULL != lbuf ){ /**we have valid filename & channel, open  */
+			sprintf(lbuf,"%s.%s",ofname,channel[c]);
+			if(0>(ofd=open(lbuf,O_RDWR|O_CREAT,S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR))) {
+				printf("Unable to open '%s' for writing\n",lbuf);
+				goto closem;
+			}
+		}	
+		if(!strcmp(CH_LIST[CURSORS],channel[c])) {
+			if(-1 == (i=read_cursors(p,ofd)) )
+				goto closem;
+		} else {
+			set_channel(p,channel[c]);
+			printf("Reading Channel %s\n",channel[c]);
+			if(-1 == (i=get_data(p)) ){
+				printf("Unable to get waveform??\n");
+				goto closem;
+			}	
+		}
+		
+		write(ofd,buf,i);	
+		if(ofd>2){
+			close(ofd);
+			ofd=1;
+		}
 	}
-	write(ofd,buf,c);
 	rtn=0;
 closem:
 	if(ofd>1)
