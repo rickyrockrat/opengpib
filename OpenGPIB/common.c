d8a39d53f29ea756b66d5cd9c909ee00258a88b3
RickyRockRat <gpib@rickyrockrat.com>
2008-08-03 06:18:42 +0000

    Moved functions from tek2gplot.c
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@9 fe987565-7590-435f-b057-69d0534b0db8

---
+++ OpenGPIB/common.c
@@ -0,0 +1,56 @@
+/** \file ******************************************************************
+\n\b File:        common.c
+\n\b Author:      Doug Springer
+\n\b Company:     DNK Designs Inc.
+\n\b Date:        08/02/2008 11:48 pm
+\n\b Description: Common functions.
+*/ /************************************************************************
+Change Log: \n
+$Log: not supported by cvs2svn $
+*/
+
+#include "common.h"
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+double get_value( char *f, char *buf)
+{
+	char tbuf[50], *find;
+	int i,k;
+	double x; 
+	
+	find=strstr(buf,f);
+	if(NULL==find)
+		return -1;
+	for (k=0,i=strlen(f)+1; k<10 && find[i] != ',' && find[i] != ';';++i,++k)
+		tbuf[k]=find[i];
+	tbuf[k]=0;
+	/*printf("Found %s->%s",f,tbuf); */
+	x=strtof(tbuf, NULL);
+	/*printf(" : %f\n",x); */
+	
+	return(x );	 
+}
+
+/***************************************************************************/
+/** Get the string following the title.
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+char * get_string( char *f, char *buf)
+{
+	char tbuf[50], *find;
+	int i,k;
+	
+	find=strstr(buf,f);
+	if(NULL==find)
+		return "NOTFOUND";
+	for (k=0,i=strlen(f)+1; k<30 && find[i] != ',' && find[i] != ';';++i,++k)
+		tbuf[k]=find[i];
+	tbuf[k]=0;
+	/*printf("Found %s->%s\n",f,tbuf); */
+	
+	return(strdup(tbuf));	 
+}
---
+++ OpenGPIB/common.h
@@ -0,0 +1,18 @@
+/** \file ******************************************************************
+\n\b File:        common.h
+\n\b Author:      Doug Springer
+\n\b Company:     DNK Designs Inc.
+\n\b Date:        08/02/2008 11:49 pm
+\n\b Description: 
+*/ /************************************************************************
+Change Log: \n
+$Log: not supported by cvs2svn $
+*/
+#ifndef _COMMON_H_
+#define _COMMON_H_ 1
+#include <string.h>
+#include <stdlib.h>
+double get_value( char *f, char *buf);
+char * get_string( char *f, char *buf);
+#endif 
+
