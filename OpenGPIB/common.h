76b8042363256de992f73dcea7972bcb7c3cc226
RickyRockRat <gpib@rickyrockrat.com>
2008-08-18 21:14:34 +0000

    Added _XOPEN_SOURCE for strdup with C99
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@25 fe987565-7590-435f-b057-69d0534b0db8

--- OpenGPIB/common.h
+++ OpenGPIB/common.h
@@ -7,11 +7,18 @@
 */ /************************************************************************
 Change Log: \n
 $Log: not supported by cvs2svn $
+Revision 1.1  2008/08/03 06:18:42  dfs
+Moved functions from tek2gplot.c
+
 */
 #ifndef _COMMON_H_
 #define _COMMON_H_ 1
+#ifdef _COMMON_
+#define _XOPEN_SOURCE 501
+#endif 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 double get_value( char *f, char *buf);
 char * get_string( char *f, char *buf);
 #endif 
