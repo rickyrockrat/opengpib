3932e1561a511e8b83130bba43dd5fd1f67ae120
RickyRockRat <gpib@rickyrockrat.com>
2008-10-06 12:45:23 +0000

    Added write_get_data, auto to init_prologix
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@52 fe987565-7590-435f-b057-69d0534b0db8

--- OpenGPIB/gpib.h
+++ OpenGPIB/gpib.h
@@ -7,6 +7,9 @@
 */ /************************************************************************
 Change Log: \n
 $Log: not supported by cvs2svn $
+Revision 1.1  2008/10/06 07:54:16  dfs
+moved from get_tek_waveform
+
 */
 #ifndef _GPIB_H_
 #define _GPIB_H_ 1
@@ -16,6 +19,7 @@ $Log: not supported by cvs2svn $
 #define BUF_SIZE 8096
 int read_string(struct serial_port *p, char *msg, int len);
 int write_string(struct serial_port *p, char *msg, int len);
+int write_get_data (struct serial_port *p, char *cmd);
 void verify(struct serial_port *p, char *msg);
-int init_prologix(struct serial_port *p, int inst_addr);
+int init_prologix(struct serial_port *p, int inst_addr, int autor);
 #endif
