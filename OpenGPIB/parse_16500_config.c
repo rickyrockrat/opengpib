85fa6bf43184ea6d2528b0a992a8802e8a6a4864
RickyRockRat <gpib@rickyrockrat.com>
2010-01-11 21:23:19 +0000

    First working data parser
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@113 fe987565-7590-435f-b057-69d0534b0db8

--- OpenGPIB/parse_16500_config.c
+++ OpenGPIB/parse_16500_config.c
@@ -30,63 +30,14 @@ Change Log: \n
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "common.h"
+#include "hp16500.h"
 
-struct block_spec {
-	uint8 blockstart[2]; /**should be #8  */
-	char blocklen_data[8];	 /**decimal len of block, stored in ascii  */
-	uint32 blocklen;
-}__attribute__((__packed__));	
-
-struct section_hdr {
-	char name[11]; /**the last byte is reserved, but usally 0  */
-	uint8 module_id;
-	uint32 block_len; /**lsb is most significant byte, i.e 0x100 is stored as 10 0, not 0 10  */
-}__attribute__((__packed__));	
-
-struct hp_config_block {
-	struct block_spec bs;
-	char *data;
-}__attribute__((__packed__));	
-
-struct section {
-	struct section_hdr hdr;
-	uint32 sz;
-	uint32 off;
-	char *data;
-}__attribute__((__packed__));	
-#define LABEL_RECORD_LEN 22
-#define LABEL_MAP_LEN 12
-struct label_map {
-	uint8 unknown1;
-	uint8 clk;
-	uint8 unknown2[4];
-	uint8 pod2_hi;
-	uint8 pod2_lo;
-	uint8 pod1_hi;
-	uint8 pod1_lo; 
-	uint8 unknown3;
-}__attribute__((__packed__));	
-
-struct labels {
-	char name[6];
-	uint8 unknown1[3];
-	uint8 polarity;
-	uint8 unknown2;
-	uint8 strange_offset;
-	uint16 strange_offsetlo;
-	uint8 unknown3[4];
-	uint8 bits;
-	uint8 enable;
-	uint8 sequence;
-	uint32 actual_offset;
-	struct label_map map;
-}__attribute__((__packed__));	
 /***************************************************************************/
 /** .
 \n\b Arguments:
 \n\b Returns:
 ****************************************************************************/
-uint32 swapbytes(uint32 in)
+uint32 swap32(uint32 in)
 {
 	uint32 out;
 	out=in<<24;
@@ -96,7 +47,26 @@ uint32 swapbytes(uint32 in)
 	/*printf("in %d out %d\n",in,out); */
 	return out;
 }
-
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+uint64 swap64(uint64 in)
+{
+	uint64 out;
+	out=in<<56;
+	out|=in>>56;
+	
+	out|=(in&0x000000000000FF00)<<40;
+	out|=(in&0x00FF000000000000)>>40;
+	out|=(in&0x0000000000FF0000)<<24;
+	out|=(in&0x0000FF0000000000)>>24;
+	out|=(in&0x00000000FF000000)<<8;
+	out|=(in&0x000000FF00000000)>>8;
+	/*printf("in %016lx out %016lx\n",in,out);  */
+	return out;
+}
 /***************************************************************************/
 /** .
 \n\b Arguments:
@@ -111,11 +81,12 @@ uint16 swap16(uint16 in)
 	return out;
 }
 /***************************************************************************/
-/** find location of section using name. Return the offset to start of header.
+/** find location of section using name. Allocate and load the data into the 
+structure.
 \n\b Arguments:
 \n\b Returns:
 ****************************************************************************/
-struct section *find_section(char *name, struct hp_config_block *blk )
+struct section *find_section(char *name, struct hp_block_hdr *blk )
 {
 	uint32 total;
   struct section *sec;
@@ -126,8 +97,8 @@ struct section *find_section(char *name, struct hp_config_block *blk )
 	for (total=0,sec->sz=1; total<blk->bs.blocklen && sec->sz>0;){
 		memcpy(&sec->hdr,(char *)(blk->data+total),sizeof(struct section_hdr));
 		sec->hdr.name[10]=0;
-		sec->sz=swapbytes(sec->hdr.block_len);
-		printf("Name %s module_id %d len %d\n",sec->hdr.name, sec->hdr.module_id,sec->sz);	
+		sec->sz=swap32(sec->hdr.block_len);
+		printf("Looking at Name %s module_id %d len %d\n",sec->hdr.name, sec->hdr.module_id,sec->sz);	
 		
 		if(!strcmp(name,sec->hdr.name)){
 			if( NULL == (sec->data=malloc(sec->sz)) )	{
@@ -150,14 +121,14 @@ struct section *find_section(char *name, struct hp_config_block *blk )
 \n\b Arguments:
 \n\b Returns:
 ****************************************************************************/
-void show_sections(struct hp_config_block *blk )
+void show_sections(struct hp_block_hdr *blk )
 {
 	uint32 total, sz;
 	struct section_hdr hdr;
 	for (total=0,sz=1; total<blk->bs.blocklen && sz>0;){
 		memcpy(&hdr,(char *)(blk->data+total),sizeof(struct section_hdr));
 		hdr.name[10]=0;
-		sz=swapbytes(hdr.block_len);
+		sz=swap32(hdr.block_len);
 		printf("Name '%s' module_id %d len %d\n",hdr.name, hdr.module_id,sz);	
 		total+=sz+16; /**16 is size of section info  */
 	}
@@ -192,49 +163,24 @@ void show_labelmaps(struct section *sec)
 	}
 	
 }
+
+
 /***************************************************************************/
 /** .
 \n\b Arguments:
 \n\b Returns:
 ****************************************************************************/
-void usage(void)
+void parse_config( char *cfname)
 {
-	printf("Usage: parse_16500_config -i file\n");
-}
-/***************************************************************************/
-/** .
-\n\b Arguments:
-\n\b Returns:
-****************************************************************************/
-int main(int argc, char *argv[])
-{
-	FILE *ofd;
-	char *ofname;
-	struct hp_config_block blk;
+	FILE *cfd;
+	struct hp_block_hdr blk;
 	struct section *sec;
-	int c;
-	ofname=NULL;
-	ofd=NULL;
-	while( -1 != (c = getopt(argc, argv, "i:h")) ) {
-		switch(c){
-			case 'i':
-				ofname=strdup(optarg);
-				break;
-			default:
-				usage();
-				return 1;
-		}
-	}
-	if(NULL == ofname){
-		usage();
-		goto closem;
-	}	
-	if(NULL == (ofd=fopen(ofname,"r"))) {
-		printf("Unable to open '%s' for writing\n",ofname);
+	if(NULL == (cfd=fopen(cfname,"r"))) {
+		printf("Unable to open '%s' for writing\n",cfname);
 		goto closem;
 	}
 	/**read in header  */
-	fread(&blk,1,10,ofd);
+	fread(&blk,1,10,cfd);
 	sscanf(blk.bs.blocklen_data,"%d",&blk.bs.blocklen);
 	printf("Blocksize is %d\n",blk.bs.blocklen);
 	/**allocate room for data  */
@@ -243,7 +189,7 @@ int main(int argc, char *argv[])
 		goto closem;
 	}
 	/**read data in  */
-	fread(blk.data,1,blk.bs.blocklen,ofd);
+	fread(blk.data,1,blk.bs.blocklen,cfd);
 	show_sections(&blk);
 	if(NULL == (sec=find_section("CONFIG    ",&blk))){
 		printf("Unable to find section 'CONFIG    '\n");
@@ -251,7 +197,6 @@ int main(int argc, char *argv[])
 	}
 	printf("First Machine is '%s', second is '%s'\n",sec->data,(char *)(sec->data+0x40));
 	/** printf("First Label is '%s'\n",(char *)(sec->data+0x27A));
-	
 	memcpy(&l,sec->data+0x27A,LABEL_RECORD_LEN);
 	l.actual_offset=swap16(l.strange_offsetlo);
 	printf("Actual map offset=0x%x ",l.actual_offset);
@@ -260,10 +205,241 @@ int main(int argc, char *argv[])
 	memcpy(&l.map,sec->data+l.actual_offset,LABEL_MAP_LEN);
 	printf("%s map is clk 0x%02x, P1 0x%02x %02x P2 0x%02x %02x\n",l.name,l.map.clk,
 						                    l.map.pod1_hi,l.map.pod1_lo,l.map.pod2_hi,l.map.pod2_lo);*/
-	
 	show_labelmaps(sec);
+	
 closem:
-	if(NULL != ofd)
-		fclose(ofd);
+	if(NULL != cfd)
+		fclose(cfd);
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void swap_analyzer_bytes(struct analyzer_data *a)
+{
+	a->data_mode=swap32(a->data_mode);
+	a->pods=swap32(a->pods);
+	a->masterchip=swap32(a->masterchip);
+	a->maxmem=swap32(a->maxmem);
+	a->sampleperiod=swap64(a->sampleperiod);
+	a->tag_type=swap32(a->tag_type);
+	a->trig_off=swap64(a->trig_off);	
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void swapbytes(struct data_preamble *p)
+{
+	p->instid=swap32(p->instid);
+	p->rev_code=swap32(p->rev_code);
+	p->chips=swap32(p->chips);
+	p->analyzer_id=swap32(p->analyzer_id);
+	swap_analyzer_bytes(&p->a1);
+  swap_analyzer_bytes(&p->a2);
+	
+	p->data_pod4_hi=swap32(p->data_pod4_hi);
+	p->data_pod3_hi=swap32(p->data_pod3_hi);
+	p->data_pod2_hi=swap32(p->data_pod2_hi);
+	p->data_pod1_hi=swap32(p->data_pod1_hi);
+	p->data_pod4_mid=swap32(p->data_pod4_mid);
+	p->data_pod3_mid=swap32(p->data_pod3_mid);
+	p->data_pod2_mid=swap32(p->data_pod2_mid);
+	p->data_pod1_mid=swap32(p->data_pod1_mid);
+	p->data_pod4_master=swap32(p->data_pod4_master);
+	p->data_pod3_master=swap32(p->data_pod3_master);
+	p->data_pod2_master=swap32(p->data_pod2_master);
+	p->data_pod1_master=swap32(p->data_pod1_master);
+	p->trig_pod4_hi=swap32(p->trig_pod4_hi);
+	p->trig_pod3_hi=swap32(p->trig_pod3_hi);
+	p->trig_pod2_hi=swap32(p->trig_pod2_hi);
+	p->trig_pod1_hi=swap32(p->trig_pod1_hi);
+	p->trig_pod4_mid=swap32(p->trig_pod4_mid);
+	p->trig_pod3_mid=swap32(p->trig_pod3_mid);
+	p->trig_pod2_mid=swap32(p->trig_pod2_mid);
+	p->trig_pod1_mid=swap32(p->trig_pod1_mid);
+	p->trig_pod4_master=swap32(p->trig_pod4_master);
+	p->trig_pod3_master=swap32(p->trig_pod3_master);
+	p->trig_pod2_master=swap32(p->trig_pod2_master);
+	p->trig_pod1_master=swap32(p->trig_pod1_master);
+	p->rtc.year=swap16(p->rtc.year);
+	/*p->rtc.month=swap16(p->rtc.month); */
+	
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void show_analyzer(struct analyzer_data *a)
+{
+	printf("datam=%d pods=%08x master=%04x maxmem=%04x samp=%ldps tagtype=%04x trig_off=%ldps\n",
+	a->data_mode, a->pods, a->masterchip, a->maxmem, a->sampleperiod,a->tag_type,a->trig_off);	
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void show_pre(struct data_preamble *p)
+{
+	struct rtc_data *r;
+	printf("ID=%d REV=%04x Chips=%04x AID=%04x\n",p->instid,p->rev_code, p->chips, p->analyzer_id);
+	printf("Analyzer 1\n");
+	show_analyzer(&p->a1);
+	printf("Analyzer 2\n");
+	show_analyzer(&p->a2);
+	printf("Valid Data Rows\n");
+	printf("4h=%d 3h=%d 2h=%d 1h=%d 4m=%d 3m=%d 2m=%d 1m=%d M4=%d M3=%d M2=%d M1=%d\n",
+	p->data_pod4_hi,p->data_pod3_hi,p->data_pod2_hi,p->data_pod1_hi,
+	p->data_pod4_mid,p->data_pod3_mid,p->data_pod2_mid,p->data_pod1_mid,
+	p->data_pod4_master,p->data_pod3_master,p->data_pod2_master,p->data_pod1_master);
+	printf("Trigger Point\n");
+	printf("4h=%d 3h=%d 2h=%d 1h=%d 4m=%d 3m=%d 2m=%d 1m=%d M4=%d M3=%d M2=%d M1=%d\n",
+	p->trig_pod4_hi,p->trig_pod3_hi,p->trig_pod2_hi,p->trig_pod1_hi,
+	p->trig_pod4_mid,p->trig_pod3_mid,p->trig_pod2_mid,p->trig_pod1_mid,
+	p->trig_pod4_master,p->trig_pod3_master,p->trig_pod2_master,p->trig_pod1_master);
+	r=&p->rtc;
+	printf("Acq time %d/%d/%d dow %d %d:%d:%d\n",
+	r->year+1990,r->month,r->day,r->dow,r->hour,r->min,r->sec);
+	/**our trace data should start right after r->sec.  */
+	/*print_data( */
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void print_data(struct data_preamble *p, struct section *sec)
+{
+	struct data_preamble *lp;
+	struct one_card_data *d;
+	char *dstart;
+	long int ps;
+	int inc,c;
+	
+	c=0;
+	ps=0;
+	inc = ONE_CARD_ROWSIZE;
+	lp=(struct data_preamble *)sec->data;
+	
+	dstart=((char *)&lp->rtc.sec - sec->data)+sec->data+1; 
+/*	dstart = (void *)((char *)(&lp->rtc.sec) - (char *)sec->data);  */
+	
+	printf("start %p last %p %ld (0x%lx) @%p\n",sec->data,&lp->rtc.sec,(long int)(&lp->rtc.sec)-(long int)sec->data,(long int)(&lp->rtc.sec)-(long int)sec->data,dstart); 
+	while (dstart < sec->data + sec->sz){
+		int i;
+		d=(struct one_card_data *)dstart;
+		printf("CH%02x CL%02x ",d->clkhi, d->clklo);
+		for (i=0; i<8; ++i){
+			char x,y;
+			if(!(i%2))
+				x='M';
+			else 
+				x='L';
+			y='0'+(9-i)/2;
+			printf("%c%c%02x ",x,y,d->pdata[i]);
+			
+		}
+		ps+=p->a1.sampleperiod;
+		printf(" %ldns\n",ps/1000);
+		dstart+=inc;
+		++c;
+		if(c>1000)
+			break;
+	}
+	
+}
+/***************************************************************************/
+/** .\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void parse_data( char *cfname)
+{
+	FILE *cfd;
+	struct hp_block_hdr blk;
+	struct section *sec;
+	struct data_preamble pre;
+	if(NULL == (cfd=fopen(cfname,"r"))) {
+		printf("Unable to open '%s' for writing\n",cfname);
+		goto closem;
+	}
+	/**read in header  */
+	fread(&blk,1,10,cfd);
+	sscanf(blk.bs.blocklen_data,"%d",&blk.bs.blocklen);
+	printf("Blocksize is %d\n",blk.bs.blocklen);
+	/**allocate room for data  */
+	if(NULL == (blk.data=malloc(blk.bs.blocklen))){
+		printf("Unable to malloc %d bytes\n",blk.bs.blocklen);
+		goto closem;
+	}
+	/**read data in  */
+	fread(blk.data,1,blk.bs.blocklen,cfd);
+	show_sections(&blk);
+	if(NULL == (sec=find_section("DATA      ",&blk))){
+		printf("Unable to find section 'DATA      '\n");
+		goto closem;
+	}
+	memcpy(&pre,sec->data,sizeof(struct data_preamble));
+	swapbytes(&pre);
+	show_pre(&pre);
+	
+	print_data(&pre,sec);
+	
+closem:
+	if(NULL != cfd)
+		fclose(cfd);
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void usage(void)
+{
+	printf("Usage: parse_16500_config <options>\n"
+	" -c filename set name of config file\n"
+	" -d filename set name of data file\n"
+	"");
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int main(int argc, char *argv[])
+{
+	char *cfname, *dfname;
+	int c;
+	dfname=cfname=NULL;
+	while( -1 != (c = getopt(argc, argv, "c:d:h")) ) {
+		switch(c){
+			case 'c':
+				cfname=strdup(optarg);
+				break;
+			case 'd':
+				dfname=strdup(optarg);
+				break;
+			default:
+				usage();
+				return 1;
+		}
+	}
+	if(NULL == cfname && NULL == dfname){
+		usage();
+		goto closem;
+	}	
+	if(NULL != cfname ){
+		parse_config(cfname);
+	}
+	if(NULL != dfname ){
+		parse_data(dfname);
+	}
+closem:
 	return 0;
 }
