436316c5e2368b835d717dc61b45f2f15b6bdfab
RickyRockRat <gpib@rickyrockrat.com>
2011-10-10 05:24:09 +0000

    Added lots of common functions, moved hp_cardlist to here, and added a main function for querying
    git-svn-id: svn://dnkdesigns.com/open-gpib/trunk@208 fe987565-7590-435f-b057-69d0534b0db8

--- OpenGPIB/hp16500.c
+++ OpenGPIB/hp16500.c
@@ -32,6 +32,7 @@ Change Log: \n
 #include "common.h"
 #include "hp16500.h"
 #include "gpib.h"
+#include <ctype.h>
 #define SIZEOF_ACTIVE_ARRAY 13
 
 struct signal_data *show_vcd_label(char *a, struct labels *l);
@@ -42,6 +43,592 @@ struct valid_bits {
 	int bytes;
 	int bits;
 };
+
+struct hp_cards hp_cardlist[]=\
+{
+	{CARDTYPE_16515A,   MODEL_16515A,  DESC_16515A},
+	{CARDTYPE_16516A,   MODEL_16516A,  DESC_16516A},
+	{CARDTYPE_16517A,   MODEL_16517A,  DESC_16517A},
+	{CARDTYPE_16518A,   MODEL_16518A,  DESC_16518A},
+	{CARDTYPE_16530A,   MODEL_16530A,  DESC_16530A},
+	{CARDTYPE_16531A,   MODEL_16531A,  DESC_16531A},
+	{CARDTYPE_16532A,   MODEL_16532A,  DESC_16532A},
+	{CARDTYPE_16534A,   MODEL_16534A,  DESC_16534A},
+	{CARDTYPE_16535A,   MODEL_16535A,  DESC_16535A},
+	{CARDTYPE_16520A,   MODEL_16520A,  DESC_16520A},
+	{CARDTYPE_16521A,   MODEL_16521A,  DESC_16521A},
+	{CARDTYPE_16522AE,  MODEL_16522AE,  DESC_16522AE},
+	{CARDTYPE_16522AM,  MODEL_16522AM,  DESC_16522AM},
+	{CARDTYPE_16511B ,  MODEL_16511B ,  DESC_16511B },
+	{CARDTYPE_16510A ,  MODEL_16510A ,  DESC_16510A },
+	{CARDTYPE_16550AE,  MODEL_16550AE,  DESC_16550AE},
+	{CARDTYPE_16550AM,  MODEL_16550AM,  DESC_16550AM},
+	{CARDTYPE_16554M ,  MODEL_16554M ,  DESC_16554M },
+	{CARDTYPE_16554E ,  MODEL_16554E ,  DESC_16554E },
+	{CARDTYPE_16540A ,  MODEL_16540A ,  DESC_16540A },
+	{CARDTYPE_16541A ,  MODEL_16541A ,  DESC_16541A },
+	{CARDTYPE_16542A ,  MODEL_16542A ,  DESC_16542A },
+	{CARDTYPE_INVALID,NULL},
+};
+
+/** Generic functions for accessing the HP16500C go here.  */
+
+
+
+
+/***************************************************************************/
+/** Returns index to info about the card type.
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int get_hp_info_card(int id)
+{
+	int i;
+	for (i=0;NULL != hp_cardlist[i].desc;++i){
+	  //fprintf(stderr,"%d - %s\n",hp_cardlist[i].type, hp_cardlist[i].desc);
+		if(id==hp_cardlist[i].type){
+			return i;
+		}
+	}
+	return -1;
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns: index into instrument array
+****************************************************************************/
+int print_card_model(int id)
+{
+	int i;
+	if(-1 != (i=get_hp_info_card(id)))
+		fprintf(stderr,"HP %s %s",hp_cardlist[i].model,hp_cardlist[i].desc);
+	return i;
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int select_hp_card(int slot, struct gpib *g)
+{
+	char *model;
+	if(NULL == g->inst )
+		model="Unknown";
+	else 
+		model=((struct card_info *)g->inst)->info->model;
+	write_get_data(g,":SELECT?");
+
+	if(slot+1 == (g->buf[0]-0x30)){
+		fprintf(stderr,"Slot %c (%s) %s already selected\n",g->buf[0]+'A'-'0'-1,model,g->buf);
+	}	else{
+		sprintf(g->buf,":SELECT %d",slot+1);
+		write_string(g,g->buf);	
+	}
+	write_get_data(g,":SELECT?");
+/*	i=write_get_data(g,":?"); */
+	fprintf(stderr,"Selected Card %c to talk to.\n",g->buf[0]+'A'-'0'-1);
+	return 0;
+
+}
+/***************************************************************************/
+/** Print list of all known cards.
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void show_known_hp_cards( void )
+{
+	int i;
+	fprintf(stderr,"List of Known HP16500 Mainframe Cards\n"
+	"Type - Model Description\n");
+	for (i=0;NULL != hp_cardlist[i].desc;++i){
+	  fprintf(stderr,"%2d - HP %s %s\n",hp_cardlist[i].type, hp_cardlist[i].model, hp_cardlist[i].desc);
+	}
+}
+
+/***************************************************************************/
+/** Make sure we are talking to the right piece of test equipment and 
+ the correct card is found.
+\n\b Arguments: if card type is -1, assume we just want to print out the slots.
+\n\b      If no is 0, we use the last card found.
+\n\b Returns: card number found.
+****************************************************************************/
+int hp16500_find_card(int cardtype, int no, struct gpib *g)
+{
+	int i,s, info,ccount;
+	int slots[5], slot=-1;
+	if(-1 != cardtype){
+		if(-1 == (info=get_hp_info_card(cardtype)) ){
+			fprintf(stderr,"Unknown card type %d\n",cardtype);
+			show_known_hp_cards();
+			return -1;
+		}	
+		printf("Looking for card %s\n",hp_cardlist[info].model);
+		if(NULL == (g->inst=calloc(1,sizeof(struct card_info))) ){
+			printf("Out of memory allocating card_info structure\n");
+			return -1;
+		}
+		((struct card_info *)g->inst)->info=&hp_cardlist[info];
+	}
+
+	fprintf(stderr,"Initializing Instrument\n");
+	if(0!=init_id(g,"*IDN?"))
+		return -1;
+	/*fprintf(stderr,"Got %d bytes\n",i); */
+	if(NULL == strstr(g->buf,"HEWLETT PACKARD,16500C")){
+		fprintf(stderr,"Unable to find 'HEWLETT PACKARD,16500C' in id string '%s'\n",g->buf);
+		return -1;
+	}
+	if(0 == write_get_data(g,":CARDCAGE?")){
+		fprintf(stderr,"Error sending Cardcage command\n");
+		return -1;
+	}
+	
+	for (s=0;g->buf[s];++s)
+		if(isdigit(g->buf[s]))
+			break;
+	for (ccount=i=0;i<5;++i){
+		slots[i]=(int)get_value_col(i,&g->buf[s]);
+		fprintf(stderr,"Slot %c = %d - ",'A'+i,slots[i]);
+		print_card_model(slots[i]);
+		fprintf(stderr,"\n");
+		if(cardtype == slots[i]){
+			++ccount;
+			if(0== no || ccount == no){
+				slot=i;
+				if(-1 != cardtype) ((struct card_info *)g->inst)->slot=i;	
+			}
+		}
+	}
+	if( -1 != cardtype){
+		if(-1 == slot){
+			fprintf(stderr,"Unable to find %s card\n",hp_cardlist[info].model);
+		}	else{
+			fprintf(stderr,"Found ");
+			print_card_model(slots[slot]);
+			fprintf(stderr," in slot %c\n",'A'+slot);	
+		}		
+		return slot;
+	}
+	return 0;
+	
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void show_hp_connection_info(void)
+{
+	printf("\nMake sure the 16550C Controller is connected to:\n"
+	" LAN for  'hpip' or\n"
+	" HPIB for 'prologixs'\n"
+	"Supported controllers\n\n"
+	"");
+	show_gpib_supported_controllers();	
+}
+
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void show_common_usage (void)
+{
+	printf("Common Options\n"
+	" -a addr set GPIB instrument address to addr (ip address automatically sets -d)\n"
+	" -d dev set path to device name (use ipaddress for hpip)\n"
+	" -m meth set access method to meth, currently hpip or prologixs\n"
+	" -n num Set card number, use for multiple cards in system. 0 selects last card found.\n"
+	" -t type Set scope type (14)\n"
+	"   Current types are 11=16530, 13=16532, 14=16534\n"
+	"");
+}
+
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int handle_common_opts(int c, char *optarg, struct hp_common_options *o)
+{
+	if(0 == c && NULL == optarg){
+		o->iaddr=7;
+		o->cardno=0;
+		o->cardtype=-1;
+		o->iaddr=7;
+	  o->dev=NULL;
+	  o->dtype=GPIB_CTL_HP16500C;
+		return 0;
+	}
+	switch(c){
+    case 'a':
+      if(is_string_number(optarg)){
+				o->iaddr=atoi(optarg);
+				o->dtype=gpib_option_to_type("prologixs");
+			} else{ /**Assume it's an IP address  */
+				o->dev=strdup(optarg);
+				o->dtype=gpib_option_to_type("hpip");
+			}
+			break;
+		case 'd':
+			o->dev=strdup(optarg);
+			break;
+		case 'm':
+			if(-1 == (o->dtype=gpib_option_to_type(optarg)))	{
+				fprintf(stderr,"'%s' method not supported\n",optarg);
+				return 1;
+			}
+			break;
+		
+		case 'n':
+			o->cardno=atoi(optarg);
+			if(o->cardno>5 || o->cardno <0){
+				fprintf(stderr,"-n: '%s' Invalid card number, must be 0-5\n",optarg);
+				return 1;
+			}
+			break;
+		
+		case 't':
+			o->cardtype=atoi(optarg);
+			if(o->cardtype != CARDTYPE_16534A &&
+				 o->cardtype != CARDTYPE_16532A &&
+			   o->cardtype != CARDTYPE_16530A){
+			  fprintf(stderr,"'-t: %s' Invalid card type\n",optarg);							
+			  return 1;
+			}
+			break;
+		}
+	return 0;
+}
+
+#if defined (MAIN_QUERY)
+
+enum {
+	QID=0,
+	QCARD,
+};
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void usage(void)
+{
+	printf("hp16500, the HP16500 Query tool\n");
+	show_common_usage();
+	printf(" -q type Set query type to: \n"
+				 "   id - just print instrument ID\n"
+	       "   cards - show cards in instrument\n"
+	       "\n"
+	"");
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int main(int argc, char * argv[])
+{
+	struct gpib *g;
+	struct hp_common_options copt;
+	int i, c, query,rtn;
+	handle_common_opts(0,NULL,&copt);
+	rtn=0;
+	i=-2;
+	query=QID;
+	while( -1 != (c = getopt(argc, argv, "q:h"HP_COMMON_GETOPS)) ) {
+		i=1;
+		switch(c){
+			case 'a':
+			case 'd':
+			case 'm':
+			case 'n':
+			case 't':
+			  if(handle_common_opts(c,optarg,&copt))
+			  	return -1;
+				break;
+			case 'h':
+				usage();
+				return 1;
+			case 'q':
+				if( !strcmp(optarg,"id"))
+					query=QID;
+				else if(!strcmp(optarg,"cards"))
+					query=QCARD;
+				else{
+					printf("Unknown query '%s'\n",optarg);
+					return 1;
+				}
+				break;
+			default:
+				usage();
+				return 1;
+		}
+	}
+	if(-2 == i){
+		usage();
+		return 1;
+	}
+	if(NULL ==copt.dev){
+		printf("Must specify device, or -a with ipaddress\n");
+		return -1;
+	}
+	if(NULL == (g=open_gpib(copt.dtype,copt.iaddr,copt.dev,1048576))){
+		fprintf(stderr,"Can't open/init controller at %s. Fatal\n",copt.dev);
+		return 1;
+	}
+	switch(query){
+		case QID:
+			if(0!=init_id(g,"*IDN?")){
+				rtn=1;
+				goto close;
+			}
+			printf(" Addr %d, Method %d, device %s\n",copt.iaddr,copt.dtype,copt.dev);
+			printf("%s\n",g->buf);
+			break;
+		case QCARD:
+			hp16500_find_card(13,copt.cardno,g);
+			break;
+	}
+close:
+	close_gpib(g);
+	return rtn;
+}
+#endif
+
+#if 0
+enum {
+		  ACCESS=1,	 /**Access method GPIB, IP, etc.  */
+			IADDR,		 /**Instrument Address 0-30, assume GPIB, if dotted, assume net. */
+			DEVICE,		 /**device - Access method   */
+}
+
+struct gpib_options {
+	int type; 				/**an ENUM from above  */
+	int optval; 			/** option value, i.e. -'a'  */
+	char description;	/**descriptive text to use with usage prints  */
+	char *val; 				/**value of option  */
+};
+
+struct gpib_optons common[]={
+	{ACCESS,'m',NULL},
+	{IADDR,'a',NULL},
+	{DEVICE,'d',NULL},
+};
+
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+void parse_options(struct options *o)
+{
+	struct gpib *g;
+	FILE *ofd,*cfd,*tfd,*vfd;
+	char *name, *ofname;
+	char *tname,*cname,*vname;
+	int i, c,inst_addr, rtn, raw,dtype,trigger,slot, speriod;
+	long int total;
+	inst_addr=7;
+	rtn=1;
+	trigger=0;
+	name=ofname=tname=cname=vname=NULL;
+	vfd=tfd=cfd=ofd=NULL;
+	raw=0;
+	dtype=GPIB_CTL_HP16500C;
+	while( -1 != (c = getopt(argc, argv, "a:c:d:hmo:pr:t:v:")) ) {
+		switch(c){
+			case 'a':
+				inst_addr=atoi(optarg);
+				break;
+			case 'c':
+				if(NULL != vname){
+					fprintf(stderr,"-v and -c are mutually exclusive.\n");
+					usage();
+					return 1;
+				}
+				cname=strdup(optarg);
+				break;
+			case 'd':
+				name=strdup(optarg);
+				break;
+			case 'h':
+				usage();
+				return 1;
+			case 'm':
+				if(-1 == (dtype=gpib_option_to_type(optarg)))	{
+					fprintf(stderr,"'%s' method not supported\n",optarg);
+					return -1;
+				}
+				break;
+			case 'o':
+				ofname=strdup(optarg);
+				break;
+			case 'p':
+				raw=1;
+				break;
+			case 'r':
+				speriod=atoi(optarg);
+				if(0!=speriod){
+					if(-1 ==validate_sampleperiod(speriod)){
+						fprintf(stderr,"Invalid period %dns (must be 2,4,8 or 8ns increments)\n",speriod);
+						return -1;
+					}	
+				}
+				
+				trigger=1;
+				break;
+			case 't':
+				if(NULL != vname){
+					fprintf(stderr,"-v and -t are mutually exclusive.\n");
+					usage();
+					return 1;
+				}
+				tname=strdup(optarg);
+				break;
+			case 'v':
+#ifdef LA2VCD_LIB 
+				if(NULL != tname || NULL != cname){
+					fprintf(stderr,"-v and -c/-tare mutually exclusive.\n");
+					usage();
+					return 1;
+				}				
+				if(NULL ==(vname=malloc( (strlen(optarg)+5)*3 ))){
+					fprintf(stderr,"Unable to malloc filename space for '%s'\n",optarg);
+					return 1;
+				}
+				i=1+sprintf(vname,"%s.vcd",optarg);
+				
+				cname=&vname[i];
+				i+=1+sprintf(cname,"%s.cfg",optarg);
+				tname=&vname[i];
+				sprintf(tname,"%s.dat",optarg);
+#else
+				fprintf(stderr,"-v not supported. Re-build with LA2VCD_LIB=/path/to/lib\n");
+				return 1;
+#endif
+				break;
+			default:
+				usage();
+				return 1;
+		}
+	}	
+}
+#endif
+
+/**  This section contains oscilliscope interfacing  */
+
+
+/***************************************************************************/
+/** FMT,TYPE,Points,Count,Xinc,Xorigin,Xref,Yinc,Yorigin,Yref.
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int oscope_parse_preamble(struct gpib *g, struct hp_scope_preamble *h)
+{
+	h->fmt=get_value_col(0,g->buf);
+	h->points=get_value_col(2,g->buf);
+	h->xinc=get_value_col(4,g->buf);
+	h->xorg=get_value_col(5,g->buf);
+	h->xref=get_value_col(6,g->buf);
+	h->yinc=get_value_col(7,g->buf);
+	h->yorg=get_value_col(8,g->buf);
+	h->yref=get_value_col(9,g->buf);
+	return 0;
+}
+
+/***************************************************************************/
+/** returns the value of the digits at the end. 0 is invalid channel number.
+\n\b Arguments:
+\n\b Returns: 0 on error, channel no on success.
+****************************************************************************/
+int check_oscope_channel(char *ch)
+{
+	int i,b;
+	if(NULL ==ch)
+		return 0;
+	for (b=-1,i=0;ch[i];++i){
+		if(isdigit(ch[i])){
+			b=i;
+			break;
+		}
+	}
+	if( -1 == b ){
+		fprintf(stderr,"Channel '%s' is invalid\n",ch);
+		return 0;
+	}
+	return atoi(&ch[b]);
+}
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int oscope_get_preamble(struct gpib *g,char *ch, struct hp_scope_preamble *h)
+{
+	int i;
+	if(NULL == ch)
+		return -1;
+	if(0 == (i=check_oscope_channel(ch)))
+		return -1;
+	/*fprintf(stderr,"GetPre %d\n",i);	  */
+	sprintf(g->buf,":WAV:SOUR CHAN%d;PRE?",i);
+	if( write_get_data(g,g->buf) <=0){
+		fprintf(stderr,"No data from preamble (%d)\n",i);
+		return 0; 
+	}
+		
+	oscope_parse_preamble(g,h);
+	return 1;
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int get_oscope_data(struct gpib *g, char *ch)
+{
+	int i;
+	if(NULL == ch)
+		return -1;
+	if(0 == (i=check_oscope_channel(ch)))
+		return -1;
+/*	fprintf(stderr,"GetData %d\n",i); */
+	sprintf(g->buf,":WAV:SOUR CHAN%d;DATA?",i);
+	i=write_get_data(g,g->buf);	
+	return i;
+}
+
+/***************************************************************************/
+/** .
+\n\b Arguments:
+\n\b Returns:
+****************************************************************************/
+int init_oscope_instrument(int cardtype, int cardno, struct gpib *g)	 
+{
+	int slot,i;
+	
+	if(-1 == (slot=hp16500_find_card(cardtype, cardno,g)) ){
+		fprintf(stderr,"Unable to find timebase card\n");
+		return -1;
+	}
+	select_hp_card(slot, g);
+	
+	i=write_get_data(g,":WAV:REC FULL;:WAV:FORM BYTE;:SELECT?");
+	fprintf(stderr,"Selected Card %s to talk to.\n",g->buf);
+
+	return i;
+/*34,-1,12,12,11,1,0,5,5,5 */
+	/** :WAV:REC FULL;:WAV:FORM ASC;:SELECT? */
+}
+
+/**this is for the logic analyzer functionality.   */
 
 /***************************************************************************/
 /** Make sure the sample period is valid.
@@ -59,23 +646,7 @@ int validate_sampleperiod(uint32 p)
 		return -1;
 	return 0;
 }
-/***************************************************************************/
-/** .
-\n\b Arguments:
-\n\b Returns: index into instrument array
-****************************************************************************/
-int print_card_model(int id, struct hp_cards *h)
-{
-	int i;
-	for (i=0;NULL != h[i].desc;++i){
-	  //fprintf(stderr,"%d - %s\n",h[i].type, h[i].desc);
-		if(id==h[i].type){
-			fprintf(stderr,"%s",h[i].desc);
-			return i;
-		}
-	}
-	return -1;
-}
+
 /***************************************************************************/
 /** .
 \n\b Arguments:
@@ -1013,4 +1584,8 @@ struct signal_data *show_la2vcd(struct data_preamble *pre, struct section *sec, 
 	
 	return show_vcd_label(NULL,NULL);
 }
+
+
+
+
 
